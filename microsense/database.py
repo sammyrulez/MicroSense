44fd812321f4ebb2c61429183318b6578d85470e
Sam <sam@dev.com>
2024-05-12 10:49:03 +0200

    Basic metric persistence

---
+++ microsense/database.py
@@ -0,0 +1,13 @@
+import sqlite3
+from typing import List
+con = sqlite3.connect("metrics.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS  metrics (timestamp INT, value REAL, metric VARCHAR(255))")
+
+def insert_metric(timestamp, value, metric):
+    cur.execute("INSERT INTO metrics (timestamp, value, metric) VALUES (?, ?, ?)", (timestamp, value, metric))
+    con.commit()
+
+def get_metrics(metric:str) -> List:
+    cur.execute("SELECT * FROM metrics WHERE metric = ?", (metric,))
+    return cur.fetchall()
--- microsense/main.py
+++ microsense/main.py
@@ -1,5 +1,26 @@
-from fastapi import FastAPI  
+from http.client import CREATED
+from typing import List
+from fastapi import FastAPI, Response  
 app = FastAPI()   
+from microsense import database
+from dataclasses import dataclass
+from datetime import datetime
+
 @app.get("/") 
 async def main_route():     
   return {"message": "Hey, It is me Goku"}
+
+@dataclass
+class Metric:
+   timestamp: datetime
+   value: float
+   name: str
+    
+@app.post("/metrics")
+async def metics( metric:Metric) -> Response:
+    database.insert_metric(metric.timestamp.timestamp(), metric.value,metric.name )
+    return Response(content="Ok", status_code=CREATED ,media_type="text/plain")
+
+@app.get("/metrics/{metric}")
+async def get_metrics(metric:str)-> List:
+    return database.get_metrics(metric)
